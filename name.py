f2cdd2f022322f1655ea1c8d7185e08183e85058
mcenazy159 <73200403+G0d1y@users.noreply.github.com>
2024-11-09 22:30:14 +0330

    Update name.py

--- name.py
+++ name.py
@@ -4,7 +4,6 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-# Load configuration
 with open('config3.json') as config_file:
     config = json.load(config_file)
 
@@ -27,17 +26,15 @@ async def text_handler(client, message: Message):
     base_name = match.group(2)
 
     resolutions = ["360p", "480p", "540p", "720p", "1080p"]
-    episode_list = []
 
-    for i in range(1, episode_count + 1):
-        for res in resolutions:
+    for res in resolutions:
+        count = 0
+        for i in range(1, episode_count + 1):
             episode_name = f"@{base_name}.E{i:02}.{res}"
-            episode_list.append(f'`{episode_name}` \n')
+            await message.reply(f'`{episode_name}`')
+            count += 1
 
-    for i in range(0, len(episode_list), 5):
-        batch = episode_list[i:i + 5]
-        response_text = "\n".join(batch)
-        await message.reply(response_text)
-        await asyncio.sleep(1)
+            if count % 5 == 0:
+                await asyncio.sleep(1)
 
 app.run()
